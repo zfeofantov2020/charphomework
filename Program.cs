e46b7ef042e6ab564b9c605ae3be4cd1df7940d8
Eugene <zfeofantov@gmail.com>
2022-09-24 15:42:18 +0100

    Home work #2 Task#10

--- Program.cs
+++ Program.cs
@@ -1,2 +1,22 @@
-﻿
-Console.WriteLine("Third Task");
+﻿// >Задача 10: Напишите программу, которая принимает на
+// вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+
+// >>456 -> 5
+// 782 -> 8
+// 918 -> 1
+
+Console.WriteLine("Task #10");
+Console.Clear();
+Console.WriteLine("Insert number from 100 to 999");
+int x = Convert.ToInt32(Console.ReadLine());
+if((x <  100) || (x > 1000))
+{
+    Console.WriteLine("Your number is wrong :try again");
+}
+else{
+
+    double newX = (x / 10) % 10;
+    Console.WriteLine(newX);
+}
+
+
